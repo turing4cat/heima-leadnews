461c4d6b0e621c276d026af02e9f0a0115df0608
李一博 <turing_cat@qq.com>
2020-11-16 11:30:50 +0800

    后台管理模块---使用nacos作为注册中心
    Signed-off-by: 李一博 <turing_cat@qq.com>

--- heima-leadnews-admin/src/main/java/com/heima/admin/AdminApplication.java
+++ heima-leadnews-admin/src/main/java/com/heima/admin/AdminApplication.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.heima.admin.mapper")
+@EnableDiscoveryClient
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class,args);
